95466b7a15f34f19e27c502ce82569393a8f3705
AbdullahSl98 <abdullahslimani98@gmail.com>
2021-09-03 17:26:28 +1000

    some changes

--- asd-ems/src/App.js
+++ asd-ems/src/App.js
@@ -1,12 +1,20 @@
-import Login from './Login';
-import { Heading, Flex } from '@chakra-ui/react';
+import Login from "./Login";
+import Dashboard from "./Dashboard";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Heading, Flex } from "@chakra-ui/react";
 
 function App() {
   return (
-    <Flex height="100vh" alignItems="center" justifyContent="center" direction="column">
-      <Heading mb={20}> Employee Management System </Heading>
-      <Login />
-    </Flex>
+    <Router>
+      <div>
+        <Login />
+      </div>
+      <Switch>
+        <Route path="/Dashboard">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- asd-ems/src/Login.js
+++ asd-ems/src/Login.js
@@ -1,51 +1,66 @@
-import { Button, Flex, Heading, Input, FormControl, Spacer } from '@chakra-ui/react';
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {
+  Button,
+  Flex,
+  Heading,
+  Input,
+  FormControl,
+  Spacer,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { useHistory, Link } from "react-router-dom";
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const history = useHistory();
 
   const validateForm = () => {
     return username.length > 0 && password.length > 0;
   };
 
-  const toPage = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-  };
-
-  const handleSubmit = event => {
-    event.preventDefault();
+    history.push("/Dashboard");
   };
 
   return (
-    <Flex direction="column" background="gray.100" p={12} rounded={6}>
-      <Heading mb={6}>Log in</Heading>
-      <FormControl onSubmit={handleSubmit}>
-        <Input
-          placeholder="username"
-          variant="filled"
-          onChange={e => setUsername(e.target.value)}
-          mb={3}
-        />
-        <Input
-          placeholder="password"
-          variant="filled"
-          onChange={e => setPassword(e.target.value)}
-          type="password"
-          mb={6}
-        />
-      </FormControl>
-      <Flex direction="row">
-        <Button colorScheme="teal" disabled={!validateForm()} onClick={handleSubmit}>
-          Log in
-        </Button>
-        <Spacer />
-        <Button colorScheme="teal" disabled={true}>
-          Create an Account
-        </Button>
+    <Flex
+      height="100vh"
+      alignItems="center"
+      justifyContent="center"
+      direction="column"
+    >
+      <Heading mb={20}> Employee Management System </Heading>
+      <Flex direction="column" background="gray.100" p={12} rounded={6}>
+        <Heading mb={6}>Log in</Heading>
+        <FormControl onSubmit={handleSubmit}>
+          <Input
+            placeholder="username"
+            variant="filled"
+            onChange={(e) => setUsername(e.target.value)}
+            mb={3}
+          />
+          <Input
+            placeholder="password"
+            variant="filled"
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            mb={6}
+          />
+        </FormControl>
+        <Flex direction="row">
+          <Button
+            colorScheme="teal"
+            disabled={!validateForm()}
+            onClick={handleSubmit}
+          >
+            Log in
+          </Button>
+          <Spacer />
+          <Button colorScheme="teal" disabled={true}>
+            Create an Account
+          </Button>
+        </Flex>
       </Flex>
     </Flex>
   );
