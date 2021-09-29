9c18a269dee7bbaf6fe4f829c107507087ba0261
abdullahSl98 <abdullahslimani98@gmail.com>
2021-09-29 15:26:45 +1000

    made some changes for mongoDB

--- asd-ems/tests/addperson-mongo.js
+++ asd-ems/tests/addperson-mongo.js
@@ -1,10 +1,15 @@
+// EXAMPLE OF HOW TO USE MONGDODB URL
+
 const mongoose = require('mongoose');
 
+// MONGODB URL
 const url =
   'mongodb+srv://esd-ems:esd-ems@cluster0.qyzul.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
+// CONNECTS TO DATABASE
 mongoose.connect(url);
 
+// DEFINES THE VALUES OF THE SCHEMA
 const personSchema = new mongoose.Schema({
   name: String,
   age: Number,
@@ -12,19 +17,37 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema);
 
+// CREATING AND SAVING OBJECTS
 const person = new Person({
   name: 'John',
   age: 30,
 });
 
+// ADD NEW PERSON
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+var choice = 0;
+
+readline.question('View Database (1), Add Person to Database (2)', c => {
+  c = choice;
+  readline.close();
+});
+
+if (choice === 1) {
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person);
+    });
+    mongoose.connection.close();
+  });
+} else if (choice === 2) {
+
+}
+
 // person.save().then(() => {
 //   console.log('person saved!');
 //   mongoose.connection.close();
 // });
-
-Person.find({}).then(result => {
-  result.forEach(person => {
-    console.log(person);
-  });
-  mongoose.connection.close();
-});
