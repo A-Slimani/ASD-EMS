6a6eca0a415555b88229490a70c79b6d6b41ece9
abdullahSl98 <abdullahslimani98@gmail.com>
2021-10-06 10:04:59 +1100

    minor changes

--- asd-ems-frontend/tests/sum.test.js
+++ asd-ems-frontend/tests/sum.test.js
@@ -3,3 +3,9 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+describe('addition test (ABDULLAH SLIMANI)', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+})
