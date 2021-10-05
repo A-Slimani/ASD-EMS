d573458de336e369bcaca54c3b7be6ca94e5aca9
abdullahSl98 <abdullahslimani98@gmail.com>
2021-10-05 11:29:55 +1100

    completed employee update feature

--- asd-ems-frontend/src/UpdateEmployee.js
+++ asd-ems-frontend/src/UpdateEmployee.js
@@ -1,21 +1,58 @@
-import './style.css';
-import WebLayout from './components/WebLayout'
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 import { useRouteMatch } from 'react-router-dom';
-import employeeService from './services/Employee'
+import WebLayout from './components/WebLayout';
+import employeeService from './services/Employee';
+import './style.css';
 
 const Content = () => {
   const match = useRouteMatch('/UpdateEmployee/:id');
-  const [employee, setEmployee] = useState()
+  const [employee, setEmployee] = useState({
+    fname: '',
+    lname: '',
+    dob: '',
+    phoneno: '',
+    username: '',
+    pwd: '',
+    accname: '',
+    accnum: '',
+    accbsb: '',
+    address: '',
+    suburb: '',
+    state: '',
+    pcode: '',
+    employdate: '',
+    dept: '',
+    employmentType: '',
+  });
 
   const showEmployee = () => {
     return employee !== undefined ? employee : "";
   }
 
+ const {
+    fname, lname, dob, phoneno,
+    username, pwd,
+    accname, accnum, accbsb,
+    address, suburb, state, pcode,
+    employdate, dept, employtype
+  } = employee;
 
   useEffect(() => {
     employeeService.get(match.params.id).then(e => setEmployee(e))
   }, [])
+
+  console.log("employee test: ", employee)
+  console.log("match params id type", typeof match.params.id)
+
+  const onInputChange = e => {
+    setEmployee({...employee, [e.target.name]: e.target.value})
+  }
+
+  const onSubmit = async e => {
+    await axios.put(`http://localhost:3001/employees/${match.params.id}`, employee)
+    window.location = "/UserList"
+  }
 
   function goBack(e) {
     e.preventDefault();
@@ -31,6 +68,7 @@ const Content = () => {
     e.preventDefault();
     var s = window.confirm("Do you update the information with entered data?\n\nSelect OK to proceed\n\nSelect CANCEL to remain on the page");
     if (s === true) {
+      console.log(document.forms)
       var fname = document.forms["updateform"]["fname"].value;
       var lname = document.forms["updateform"]["lname"].value;
       var dob = document.forms["updateform"]["dob"].value;
@@ -39,7 +77,7 @@ const Content = () => {
       var phoneno = document.forms["updateform"]["phoneno"].value;
       var accname = document.forms["updateform"]["accname"].value;
       var accnum = document.forms["updateform"]["accnum"].value;
-      var accbsb = document.forms["updateform"]["bsb"].value;
+      var accbsb = document.forms["updateform"]["accbsb"].value;
       var address = document.forms["updateform"]["address"].value;
       var suburb = document.forms["updateform"]["suburb"].value;
       var state = document.forms["updateform"]["state"].value;
@@ -66,95 +104,213 @@ const Content = () => {
       else if (dept === "") { alert("Department must be select"); }
       else if (dob > employdate) { alert("Invalid date of birth and employment date"); }
       else if ((new Date().getFullYear() - new Date(dob).getFullYear()) <= 18) { alert("Employee age must be 18 or over"); }
-      else { window.location = "./UserList"; }
+      else { onSubmit() }
     }
+    else {document.getElementById("updateform").reset()}
   }
 
   return (
-   <div>
-     <button style={{ float: 'left' }} type='submit' className="update" onClick={goBack} > Back </button> <br />
-     <form id="updateform" name="updateform" >
-       <h1 style={{ textAlign: 'center', fontSize: 30, }}> Edit <b>{showEmployee().fname + " " + showEmployee().lname}</b> details </h1>
+  <>
+      <div>
+        <form id='updateform' name='updateform' onSubmit={e => onSubmit(e)}>
+          <h1 style={{ textAlign: 'center', fontSize: 30, fontWeight: 'bold', }}> Add New User </h1>
+          <p style={{ textAlign: 'center' }}> {' '} Please fill out the details below, all fields are required{' '} </p>
 
-       <label> First Name: </label>
-       <small>Up to 255 Characters </small> <p> </p>
-       <input type="text" defaultValue={showEmployee().fname} name="fname" className="formtextfield" required />{' '}
-       <br />
-       <label> Last Name: </label> <small>Up to 255 Characters </small> <p></p>
-       <input type="text" defaultValue={showEmployee().lname} name="lname" className="formtextfield" required />{' '}
-       <br />
-       <label> Date of Birth: </label> <small> Use the calendar on the right </small>{' '}
-       <p />
-       <input type="date" value={showEmployee().dob} name="dob" className="formtextfield" required />{' '}
-       <br />
-       <label> Username: </label>
-       <small>
-         After @ must be the system name 'EMS' followed by department initial: HR - Human
-         Resource, OP - Operation, MK - Marketing, FN - Finance{' '}
-       </small>
-       <p />
-       <input type="email" defaultValue={showEmployee().username} name="username" pattern="[a-z0-9._%+-]+@EMS[A-Z]{2,}$" className="formtextfield" required />{' '}
-       <br />
-       <label> Password: </label>{' '}
-       <small> Maximum password length is 16 Characters </small> <p />
-       <input type="password" placeholder="Retype Password" maxlength="16" name="pwd" className="formtextfield" required />{' '}
-       <br />
-       <label> Contact Number: </label> <small> Up to 10 digits </small> <p />
-       <input type="tel" defaultValue={showEmployee().phoneno} name="phoneno" maxlength="10" className="formtextfield" required />{' '}
-       <br />
-       <label> Bank Account Name: </label> <small> Up to 255 Characters </small>
-       <p />
-       <input type="text" defaultValue={showEmployee().accname} name="accname" className="formtextfield" required />{' '}
-       <br />
-       <label> Bank Account Number: </label>
-       <small> Up to 10 Digits </small>
-       <p />
-       <input type="text" defaultValue={showEmployee().accnum} name="accnum" minLength="6" maxLength="10" className="formtextfield" required />{' '}
-       <br />
-       <label> BSB: </label>
-       <small> Up to 6 Digits </small>
-       <p />
-       <input type="text" defaultValue={showEmployee().accbsb} name="bsb" maxLength="6" className="formtextfield" required />{' '}
-       <br />
-       <label> Address: </label> <small>Up to 255 Characters </small> <p />
-       <input type="text" defaultValue={showEmployee().address} maxlength="255" name="address" className="formtextfield" required />{' '}
-       <br />
-       <label> Suburb: </label> <small>Up to 255 Characters </small> <p />
-       <input type="text" defaultValue={showEmployee().suburb} maxlength="255" name="suburb" className="formtextfield" required />{' '}
-       <br />
-       <label> State: </label> <small> State code up 3 Character </small> <p />
-       <input type="text" defaultValue={showEmployee().state} name="state" maxlength="3" className="formtextfield" required />{' '}
-       <br />
-       <label> Post Code: </label> <small> Up 4 Digits </small> <p />
-       <input type="text" defaultValue={showEmployee().pcode} name="pcode" maxlength="4" className="formtextfield" required />{' '}
-       <p />
-       <label> Employment Type: </label> <p />
-       <input type="radio" id="html" name="employtype" value="fulltime" checked />
-       <label for="fulltime"> Full Time </label> <p />
-       <input type="radio" id="css" name="employtype" value="parttime" />
-       <label for="parttime"> Part Time </label>
-       <p />
-       <input type="radio" id="javascript" name="employtype" value="casual" />
-       <label for="casual"> Casual Worker </label> <p />
-       <label> Employment Date: </label> <small> Use the calendar on the right </small>{' '}
-       <p />
-       <input type="date" defaultvalue={showEmployee().employdate} name="employdate" className="formtextfield" required />{' '}
-       <p />
-       <label> Department: </label> <br />
-       <select name="dept" className="formtextfield" required>
-         <option value="HR"> Human Resource </option>
-         <option value="finance"> Finance </option>
-         <option value="marketing"> Marketing </option>
-         <option value="operation"> Operation </option>
-       </select>{' '}
-       <p />
-       <div style={{ textAlign: 'center', paddingTop: 10 }}>
-         <button className="button" id="updatesubmit" type="submit" onClick={handleSubmit}>
-           Update <b> {showEmployee().fname + " " + showEmployee().lname}</b> Profile
-         </button>{' '}
-       </div>
-     </form>
-   </div>
+          <label> First Name: </label>
+          <small>Up to 255 Characters </small> <p> </p>
+          <input
+            type="text"
+            defaultValue={showEmployee().fname}
+            name="fname"
+            className="formtextfield"
+            value={fname}
+            onChange={e => onInputChange(e)}
+          />{' '}
+          <br />
+
+          <label> Last Name: </label> <small>Up to 255 Characters </small> <p></p>
+          <input
+            type="text"
+            name="lname"
+            defaultValue={showEmployee().lname}
+            className="formtextfield"
+            value={lname}
+            onChange={e => onInputChange(e)}
+          />{' '}
+          <br />
+
+          <label> Date of Birth: </label>{' '}
+          <small> Use the calendar on the right </small> <p />
+          <input
+            type="date"
+            defaultValue={showEmployee().dob}
+            name="dob"
+            className="formtextfield"
+            value={dob}
+            onChange={e => onInputChange(e)}
+          />{' '}
+          <br />
+
+          <label> Username: </label> <br />
+          <small>
+            <i> After @ must be the system name 'EMS' followed by one department  initial:
+              HR - Human Resource, OP - Operation, MK - Marketing, FN - Finance </i>
+          </small>{' '}
+          <p />
+          <input
+            type="email"
+            defaultValue={showEmployee().username}
+            name="username"
+            className="formtextfield"
+            value={username}
+            onChange={e => onInputChange(e)}
+          />{' '}
+          <br />
+
+          <label> Password: </label>{' '}
+          <small> Maximum password length is 16 Characters </small> <p />
+          <input
+            type="password"
+            defaultValue={showEmployee().pwd}
+            maxLength="16" name="pwd"
+            value={pwd}
+            className="formtextfield"
+            onChange={e => onInputChange(e)}
+          />{' '}
+          <br />
+
+          <label> Contact Number: </label> <small> Up to 10 digits </small> <p />
+          <input
+            type="text"
+            defaultValue={showEmployee().phoneno}
+            name="phoneno"
+            maxLength="10"
+            className="formtextfield"
+            value={phoneno}
+            onChange={e => onInputChange(e)}
+          />{' '}
+          <br />
+
+          <label> Bank Account Name: </label> <small> Up to 255 Characters </small>
+          <p />
+          <input
+            type="text"
+            defaultValue={showEmployee().accname}
+            name="accname"
+            className="formtextfield"
+            value={accname}
+            onChange={e => onInputChange(e)}
+          />{' '}
+          <br />
+
+          <label> Bank Account Number: </label>
+          <small> Up to 10 Digits </small>
+          <p />
+          <input
+            type="text"
+            defaultValue={showEmployee().accnum}
+            name="accnum"
+            minLength="6" maxLength="10"
+            className="formtextfield"
+            value={accnum}
+            onChange={e => onInputChange(e)}
+          />{' '}
+          <br />
+
+          <label> BSB: </label>
+          <small> Up to 6 Digits </small>
+          <p />
+          <input
+            type="text"
+            defaultValue={showEmployee().accbsb}
+            name="accbsb" maxLength="6"
+            className="formtextfield"
+            value={accbsb}
+            onChange={e => onInputChange(e)}
+          />{' '}
+          <br />
+
+          <label> Address: </label> <small> Up to 255 Characters </small> <p />
+          <input
+            type="text"
+            defaultValue={showEmployee().address}
+            maxLength="255"
+            name="address"
+            className="formtextfield"
+            value={address}
+            onChange={e => onInputChange(e)}
+          />{' '}
+          <br />
+
+          <label> Suburb: </label> <small>Up to 255 Characters </small> <p />
+          <input
+            type="text"
+            defaultValue={showEmployee().suburb}
+            maxLength="255"
+            name="suburb"
+            className="formtextfield"
+            value={suburb}
+            onChange={e => onInputChange(e)}
+          />{' '}
+          <br />
+
+          <label> State: </label> <small> Up 3 Character </small> <p />
+          <input
+            type="text"
+            defaultValue={showEmployee().state}
+            name="state" maxLength="3"
+            className="formtextfield"
+            value={state}
+            onChange={e => onInputChange(e)}
+          />{' '}
+          <br />
+
+          <label> Post Code: </label> <small> Up 4 Digits </small> <p />
+          <input
+            type="text"
+            defaultValue={showEmployee().pcode}
+            name="pcode" maxLength="4"
+            className="formtextfield"
+            value={pcode}
+            onChange={e => onInputChange(e)}
+          />{' '}
+          <p />
+
+          <label> Employment Type: </label> <p />
+          <select name="employtype" className="formtextfield" value={employtype} defaultValue={showEmployee().employtype}  onChange={e => onInputChange(e)} >
+            <option value="select"> -- Select one -- </option>
+            <option value="Full-Time"> Full-Time </option>
+            <option value="Part-Time"> Part-Time </option>
+            <option value="Casual"> Casual </option>
+          </select>{' '}
+
+          <label> Employment Date: </label>{' '}
+          <small> Use the calendar on the right </small> <p />
+          <input
+            type="date"
+            defaultValue={showEmployee().employdate}
+            name="employdate"
+            value={employdate}
+            className="formtextfield"
+            onChange={e => onInputChange(e)} />{' '}
+          <p />
+
+          <label> Department: </label> <br />
+          <select name="dept" className="formtextfield" value={dept} defaultValue={showEmployee().dept} onChange={e => onInputChange(e)} required>
+            <option value="select"> -- Select one -- </option>
+            <option value="finance"> Finance </option>
+            <option value="HR"> Human Resource </option>
+            <option value="marketing"> Marketing </option>
+            <option value="operation"> Operation </option>
+          </select>{' '}
+          <p />
+
+          <div style={{ textAlign: 'center', paddingTop: 10 }}>
+            <button type="submit" id="submit" className="button" onClick={handleSubmit} > Update <b>{showEmployee().username}</b> </button>{' '}
+          </div>
+        </form>
+      </div>
+    </> 
   );
 };
 
