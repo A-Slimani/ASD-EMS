34686b3dca606c6cea4445fbb0d6897ab872e9d2
abdullahSl98 <abdullahslimani98@gmail.com>
2021-10-09 12:34:55 +1100

    minor changes

--- asd-ems-frontend/src/ComplaintList.js
+++ asd-ems-frontend/src/ComplaintList.js
@@ -1,7 +1,9 @@
-import { Button, Divider, Space, Table, Input } from 'antd';
+import { InputRightAddon } from '@chakra-ui/input';
+import { Button, Divider, Space, Table, Input, DatePicker } from 'antd';
 import React, { useEffect, useState } from 'react';
 import WebLayout from './components/WebLayout';
-import complaintService from "./services/Complaint";
+import complaintService from './services/Complaint';
+import './style.css'
 
 const { Column } = Table;
 
@@ -9,21 +11,43 @@ const Content = () => {
   const [complaints, setComplaints] = useState([]);
   const [nameFilter, setNameFilter] = useState('');
   const [typeFilter, setTypeFilter] = useState('');
+  const [dateFilter, setDateFilter] = useState('');
+  const [statusFilter, setStatusFilter] = useState('');
 
-  const filterByName = nameFilter === '' ? complaints : complaints.filter(c => c.fname.toLowerCase().match(nameFilter))
+  const filterByName =
+    nameFilter === ''
+      ? complaints
+      : complaints.filter(c => c.fname.toLowerCase().match(nameFilter));
 
+  const filteredComplaints =
+    nameFilter === ''
+      ? complaints
+      : complaints.filter(complaint => {
+          complaint.fname.toLowerCase().match(nameFilter) &&
+            complaint.type.match(typeFilter) &&
+            complaint.type.match(dateFilter) &&
+            complaint.type.match(statusFilter);
+        });
 
   useEffect(() => {
     complaintService.getAll().then(filecomplaint => {
-      setComplaints(filecomplaint)
-    })
-  }, [])
+      setComplaints(filecomplaint);
+    });
+  }, []);
 
   return (
     <>
       <div style={{ textAlign: 'center' }}>
-        <h1 style={{ textAlign: 'center', fontSize: 30, fontWeight: 'bold', }}> All File Complaints </h1>
-        <input type="name" placeholder="Name" name="requestedname" class="textfield" onChange={({target}) => {setNameFilter(target.value)}}/>
+        <h1 style={{ textAlign: 'center', fontSize: 30, fontWeight: 'bold' }}>
+          {' '}
+          All File Complaints{' '}
+        </h1>
+        <br />
+        <Input.Group compact>
+          <Input size="large" style={{ width: '20%' }} placeholder="Name" bordered={false}/>
+          <Input size="large" style={{ width: '20%' }} placeholder="Type" bordered={false}/>
+        </Input.Group>
+        <br />
         <p />
       </div>
 
@@ -33,16 +57,24 @@ const Content = () => {
           <Column title="First Name" dataIndex="fname" key="firstName" />
           <Column title="Last Name" dataIndex="lname" key="lastName" />
           <Column title="Type" dataIndex="complainttype" key="complainttype" />
-          <Column title="Description" dataIndex="complaintdescription" key="complaintdescription" />
+          <Column
+            title="Description"
+            dataIndex="complaintdescription"
+            key="complaintdescription"
+          />
           <Column title="Date Submitted" dataIndex="complaintdate" key="complaintdate" />
           <Column title="Status" dataIndex="status" key="status" />
-          <Column title="Options" key="id" render={(p) => (
-            <>
-              <Space split={<Divider type="vertical" />}>
-                <Button> Solved </Button>
-              </Space>
-            </>
-          )} />
+          <Column
+            title="Options"
+            key="id"
+            render={p => (
+              <>
+                <Space split={<Divider type="vertical" />}>
+                  <Button> Solved </Button>
+                </Space>
+              </>
+            )}
+          />
         </Table>
       </div>
     </>
--- asd-ems-frontend/tests/FileComplaintAbdullah.test.js
+++ asd-ems-frontend/tests/FileComplaintAbdullah.test.js
@@ -1,39 +1,27 @@
 const handleApplication = require('./Application');
-
 const handleComplaint = require('./Application');
-
 const handleConcern = require('./Application')
 
-
-
-
 test("file a complaint with correct data format input", () => {
-
     const fname = "bob";
-
     const lname = "rob";
-
     const complaintdescription = "work issue";
-
     const complaintdate = "1/1/2020";
-
     expect(handleComplaint(fname, lname, complaintdescription, complaintdate)).toBe(true);
-
 })
-
-
 
 test("file a complaint with incorrect data format input - number in textfield", () => {
-
     const fname = "bob123";
-
     const lname = "rob123";
-
     const complaintdescription = "work issue";
-
     const complaintdate = "1/1/2020";
-
     expect(handleComplaint(fname, lname, complaintdescription, complaintdate)).toBe(false);
-
 })
 
+test("file a complaint with missing details", () => {
+    const fname = "";
+    const lname = "rob123";
+    const complaintdescription = "work issue";
+    const complaintdate = "1/1/2020";
+    expect(handleComplaint(fname, lname, complaintdescription, complaintdate)).toBe(false)
+})
