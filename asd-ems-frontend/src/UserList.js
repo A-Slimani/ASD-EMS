7f8695f6eb55fc0febbf571f7186587d51bcca1e
MistyDuong <79491682+MistyDuong@users.noreply.github.com>
2021-10-13 13:20:00 +1100

    Add search function

--- asd-ems-frontend/src/UserList.js
+++ asd-ems-frontend/src/UserList.js
@@ -9,6 +9,8 @@ const { Column } = Table;
 
 const Content = () => {
   const [employees, setEmployees] = useState([])
+  const [deptFilter, setDeptFilter] = useState('');
+
   const history = useHistory();
 
   useEffect(() => {
@@ -17,16 +19,21 @@ const Content = () => {
     })
   }, [])
 
+  const filterByDept = deptFilter === '' ? employees : employees.filter(c => c.dept.toLowerCase().match(deptFilter.toLowerCase()));
+
+  //update function
   const handleEditRoute = e => {
     console.log("e.id: ", e.currentTarget.id)
     console.log(employees)
     const employee = employees.find(x => x.id.toString() === e.currentTarget.id)
-    console.log('test employee: ', employee) 
+    console.log('test employee: ', employee)
     history.push({
       pathname: `./UpdateEmployee/${employee.id}`,
     })
-  } 
-  
+  }
+
+  //execute delete payroll based on id after select the "delete" button
+  //also update the database when the function is executed
   const handleDelete = e => {
     var option = window.confirm("Do you want to delete employee with ID " + e.currentTarget.id + "? \n\n Select OK to delete or CANCEL action");
     if (option === true) {
@@ -35,23 +42,39 @@ const Content = () => {
     }
   }
 
+  //table display a the list of all created users
+  //the update button will call the update function when selected
+  //the delete button will call the delete function when selected
   return (
     <>
-      <Table dataSource={employees}>
-        <Column title="Employee ID" dataIndex="id" key="id"/>
-        <Column title="First Name" dataIndex="fname" key="firstName"/>
-        <Column title="Last Name" dataIndex="lname" key="lastName"/>
-        <Column title="Employment Type" dataIndex="employtype" key="employmentType"/>
-        <Column title="Options" key="id" render={(p) => (
-          <>
-          <Space split={<Divider type="vertical" />}>
-            <Button>view</Button>
-            <Button id={p.id} onClick={handleEditRoute}>update</Button>
-            <Button id={p.id} onClick={handleDelete}>delete</Button>
-          </Space>
-          </>
-        )}/>
-      </Table>
+      <div style={{ textAlign: 'center' }}>
+        <h1 style={{ textAlign: 'center', fontSize: 30, fontWeight: 'bold', }}> Employee List </h1>
+        <input type="text" placeholder="Department" name="requesteddept" className="textfield" onChange={({ target }) => { setDeptFilter(target.value) }} />
+        <p />
+
+        <button className="button" name="addnew" type="submit">
+          <Link to="./AddUser"> <button> Add New Employee </button></Link>
+        </button>
+      </div>
+
+      <div style={{ paddingTop: 10 }}>
+        <Table dataSource={filterByDept}>
+          <Column title="Employee ID" dataIndex="id" key="id" />
+          <Column title="First Name" dataIndex="fname" key="firstName" />
+          <Column title="Last Name" dataIndex="lname" key="lastName" />
+          <Column title="Department" dataIndex="dept" key="employmentType" />
+          <Column title="Employment Type" dataIndex="employtype" key="employmentType" />
+          <Column title="Options" key="id" render={(p) => (
+            <>
+              <Space split={<Divider type="vertical" />}>
+                <Button>view</Button>
+                <Button id={p.id} onClick={handleEditRoute}>update</Button>
+                <Button id={p.id} onClick={handleDelete}>delete</Button>
+              </Space>
+            </>
+          )} />
+        </Table>
+      </div>
     </>
   );
 };
