ba0d7e1b657b927dc8c1ba4f9d719719c1b8b069
abdullahSl98 <abdullahslimani98@gmail.com>
2021-10-18 22:58:20 +1100

    completed

--- asd-ems-frontend/src/ComplaintList.js
+++ asd-ems-frontend/src/ComplaintList.js
@@ -1,21 +1,18 @@
-import { Button, Divider, Space, Table, Input } from 'antd';
+import { Button, Divider, Space, Table, Input, Select, DatePicker } from 'antd';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import WebLayout from './components/WebLayout';
 import complaintService from './services/Complaint';
-import './css/complaintList.less';
 
 const { Column } = Table;
+const { Option } = Select;
 
 const Content = () => {
   const [complaints, setComplaints] = useState([]);
   const [nameFilter, setNameFilter] = useState('');
-  // const [typeFilter, setTypeFilter] = useState('');
-
-  const filterByName =
-    nameFilter === ''
-      ? complaints
-      : complaints.filter(c => c.fname.toLowerCase().match(nameFilter));
+  const [typeFilter, setTypeFilter] = useState('');
+  const [statusFilter, setStatusFilter] = useState('');
+  const [dateFilter, setDateFilter] = useState('');
 
   useEffect(() => {
     complaintService.getAll().then(filecomplaint => {
@@ -23,6 +20,34 @@ const Content = () => {
     });
   }, []);
 
+  // filters the values with all the inputs
+  const filteredList = () => {
+    return complaints.filter(
+      c =>
+        c.complainttype.match(typeFilter) &&
+        c.status.match(statusFilter) &&
+        c.complaintdate.match(dateFilter) &&
+        (c.fname.toLowerCase().match(nameFilter.toLowerCase()) ||
+          c.lname.toLowerCase().match(nameFilter.toLowerCase))
+    );
+  };
+
+  // updates each input
+  const handleChangeType = event => {
+    setTypeFilter(event);
+  };
+
+  const handleChangeStatus = event => {
+    setStatusFilter(event);
+  };
+
+  const handleChangeDate = event => {
+    console.log(event);
+    event !== null ? setDateFilter(event._d.toJSON().substr(0, 9)) : setDateFilter('');
+    console.log('date filter: ', dateFilter);
+  };
+
+  // Handled solved / pending complaints
   const handleSolved = async e => {
     var option = window.confirm(
       'Do you want to solve complaint with ID ' +
@@ -32,7 +57,7 @@ const Content = () => {
 
     if (option === true) {
       for (let i of complaints) {
-        if (e.currentTarget.id == i.id) {
+        if (e.currentTarget.id === i.id) {
           var concern = Object.assign({}, i);
           concern.status = 'solved';
           await axios.put(
@@ -42,7 +67,6 @@ const Content = () => {
           break;
         }
       }
-      window.location.reload();
     }
   };
 
@@ -55,17 +79,44 @@ const Content = () => {
         </h1>
         <br />
         <Input.Group compact>
-          <Input size="large" style={{ width: '20%' }} placeholder="Name" />
-          <Input size="large" style={{ width: '20%' }} placeholder="Type" />
-          <Input size="large" style={{ width: '20%' }} placeholder="Date" />
-          <Input size="large" style={{ width: '20%' }} placeholder="Status" />
+          <Input
+            size="large"
+            style={{ width: '20%' }}
+            placeholder="Name"
+            onChange={({ target }) => {
+              setNameFilter(target.value);
+            }}
+          />
+          <Select
+            id="type"
+            placeholder="type"
+            size="large"
+            style={{ width: '20%' }}
+            onChange={handleChangeType}
+            allowClear>
+            <Option value="general">general</Option>
+            <Option value="work">work</Option>
+            <Option value="personal">personal</Option>
+            <Option value="suggestion">suggestion</Option>
+          </Select>
+          <DatePicker size="large" onChange={handleChangeDate} />
+          <Select
+            id="type"
+            placeholder="status"
+            size="large"
+            style={{ width: '20%' }}
+            onChange={handleChangeStatus}
+            allowClear>
+            <Option value="pending">pending</Option>
+            <Option value="solved">solved</Option>
+          </Select>
         </Input.Group>
         <br />
         <p />
       </div>
 
       <div style={{ paddingTop: 10 }}>
-        <Table dataSource={filterByName}>
+        <Table dataSource={filteredList()}>
           <Column title="Complaint ID" dataIndex="id" key="id" />
           <Column title="First Name" dataIndex="fname" key="firstName" />
           <Column title="Last Name" dataIndex="lname" key="lastName" />
