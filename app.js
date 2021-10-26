c4a97228584e84c324ce9ebea215100b07ca79e3
AbdullahS98 <44589099+AbdullahS98@users.noreply.github.com>
2021-10-27 01:50:33 +1100

    changed baseUrl

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('./db.json');
-const middlewares = jsonServer.defaults({
-  static: './asd-ems-frontend/build',
-});
-const PORT = process.env.PORT || 8000;
-server.use(middlewares);
-server.use(
-  jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/:resource/:id/show': '/:resource/:id',
-  })
-);
-server.use(router);
-server.listen(PORT, () => {
-  console.log('server is running');
-});
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.static(path.join('asd-ems-frontend', 'build')));
+
+app.listen(port, () => console.log(`Server is running on port ${port}!`));
