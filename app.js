7b4292b49799651cdba4cc38df6694e14bbe3cd9
AbdullahS98 <44589099+AbdullahS98@users.noreply.github.com>
2021-10-27 08:46:59 +1100

    added response for routing in root app.js

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static(path.join('asd-ems-frontend', 'build')));
 
+app.get('*', (request, response) => {
+	response.sendFile(path.join('./asd-ems-frontend', 'build', 'index.html'))
+})
+
 app.listen(port, () => console.log(`Server is running on port ${port}!`));
