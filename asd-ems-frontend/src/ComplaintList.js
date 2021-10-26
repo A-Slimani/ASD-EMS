013012594f5737821e5b9b04d57e913823419508
MistyDuong <79491682+MistyDuong@users.noreply.github.com>
2021-10-27 02:37:47 +1100

    Update status function

--- asd-ems-frontend/src/ComplaintList.js
+++ asd-ems-frontend/src/ComplaintList.js
@@ -1,141 +1,143 @@
 import { Button, Divider, Space, Table, Input, Select, DatePicker } from 'antd';
 import React, { useEffect, useState } from 'react';
-import WebLayout from './components/WebLayout';
-import { Link, useHistory } from 'react-router-dom';
-import payrollService from "./services/Payroll";
-import { Redirect } from 'react-router';
 import axios from 'axios';
+import WebLayout from './components/WebLayout';
+import complaintService from './services/Complaint';
+import { Redirect } from 'react-router';
 
 const { Column } = Table;
 const { Option } = Select;
 
 const Content = () => {
-  const [payrolldb, setPayroll] = useState([]);
-
-  const [payMethodFilter, setPayMethodFilter] = useState('');
-  const [payDateFilter, setPayDateFilter] = useState('');
-  const [payNameFilter, setNameFilter] = useState('');
-
-  const history = useHistory();
+  const [complaints, setComplaints] = useState([]);
+  const [nameFilter, setNameFilter] = useState('');
+  const [typeFilter, setTypeFilter] = useState('');
+  const [statusFilter, setStatusFilter] = useState('');
+  const [dateFilter, setDateFilter] = useState('');
 
   useEffect(() => {
-    payrollService.getAll().then(payrolldb => {
-      setPayroll(payrolldb)
-    })
-  }, [])
+    complaintService.getAll().then(filecomplaint => {
+      setComplaints(filecomplaint);
+    });
+  }, []);
 
   // filters the values with all the inputs
   const filteredList = () => {
-    return payrolldb.filter(
+    return complaints.filter(
       c =>
-        c.paymethod.match(payMethodFilter) &&
-        c.paydate.match(payDateFilter) &&
-        (c.fname.toLowerCase().match(payNameFilter.toLowerCase()) ||
-          c.lname.toLowerCase().match(payNameFilter.toLowerCase))
+        c.complainttype.match(typeFilter) &&
+        c.status.match(statusFilter) &&
+        c.complaintdate.match(dateFilter) &&
+        (c.fname.toLowerCase().match(nameFilter.toLowerCase()) ||
+          c.lname.toLowerCase().match(nameFilter.toLowerCase))
     );
   };
 
   // updates each input
   const handleChangeType = event => {
-    setNameFilter(event);
+    setTypeFilter(event);
   };
 
-  const handleChangeMethod = event => {
-    setPayMethodFilter(event);
+  const handleChangeStatus = event => {
+    setStatusFilter(event);
   };
 
   const handleChangeDate = event => {
     console.log(event);
-    event !== null ? setPayDateFilter(event._d.toJSON().substr(0, 9)) : setPayDateFilter('');
-    console.log('date filter: ', payDateFilter);
+    event !== null ? setDateFilter(event._d.toJSON().substr(0, 9)) : setDateFilter('');
+    console.log('date filter: ', dateFilter);
   };
 
-  // const filterByPayMethod = payMethodFilter === '' ? payrolldb : payrolldb.filter(c => c.paymethod.toLowerCase().match(payMethodFilter.toLowerCase()));
+  // Handled solved / pending complaints
+  const handleSolved = async e => {
+    var option = window.confirm("Do you want to delete Concern with ID " + e.currentTarget.id + "? \n\n Select OK to delete or CANCEL action");
 
-  //execute delete payroll based on id after select the "delete" button
-  //also update the database when the function is executed
-  const handleDelete = e => {
-    var option = window.confirm("Do you want to delete payroll with ID " + e.currentTarget.id + "? \n\n Select OK to delete or CANCEL action");
     if (option === true) {
-      axios.delete(`http://localhost:3001/payrolldb/${e.currentTarget.id}`);
-      window.location.reload();
+      for (let i of complaints) {
+        if (e.currentTarget.id == i.id) {
+          var concern = Object.assign({}, i);
+          concern.status = "Solved";
+          await axios.put(`http://localhost:3001/filecomplaint/${e.currentTarget.id}`, concern);
+          break;
+        }
+      }
+      window.location = "./ConcernList"
     }
   }
 
-  const handleEditRoute = e => {
-    console.log("e.id: ", e.currentTarget.id)
-    console.log(payrolldb)
-
-    const payroll = payrolldb.find(x => x.id.toString() === e.currentTarget.id)
-
-    console.log('test payroll: ', payroll)
-    history.push({
-      pathname: `/UpdatePayroll/${payroll.id}`,
-    })
-  }
-
-  //table display a the list of all created payrols
-  //the update button will call the update function when selected
-  //the delete button will call the delete function when selected
   if (localStorage.getItem("id") !== null) {
     return (
       <>
         <div style={{ textAlign: 'center' }}>
-          <h1 style={{ textAlign: 'center', fontSize: 30, fontWeight: 'bold', }}> Payroll History </h1>
+          <h1 style={{ textAlign: 'center', fontSize: 30, fontWeight: 'bold' }}>
+            {' '}
+            All File Complaints{' '}
+          </h1>
+          <br />
+          <Input.Group compact>
+            <Input
+              size="large"
+              style={{ width: '20%' }}
+              placeholder="First Name"
+              onChange={({ target }) => {
+                setNameFilter(target.value);
+              }}
+            />
+            <Select
+              id="type"
+              placeholder="Complaint Type"
+              size="large"
+              style={{ width: '20%' }}
+              onChange={handleChangeType}
+              allowClear>
+              <Option value="General">General</Option>
+              <Option value="Work">Work</Option>
+              <Option value="Personal">Personal</Option>
+              <Option value="Suggestion">Suggestion</Option>
+            </Select>
+            <DatePicker size="large" onChange={handleChangeDate} />
+            <Select
+              id="type"
+              placeholder="Status"
+              size="large"
+              style={{ width: '20%' }}
+              onChange={handleChangeStatus}
+              allowClear>
+              <Option value="Pending">Pending</Option>
+              <Option value="Solved">Solved</Option>
+            </Select>
+          </Input.Group>
+          <br />
           <p />
-          <div style={{ textAlign: 'center' }}>
-            <Input.Group compact>
-              <Input
-                size="large"
-                style={{ width: '20%' }}
-                placeholder="Name"
-                onChange={({ target }) => {
-                  setNameFilter(target.value);
-                }}
-              />
-
-              <Select
-                id="type"
-                placeholder="Payment Method"
-                size="large"
-                style={{ width: '20%' }}
-                onChange={handleChangeMethod}
-                allowClear>
-                <Option value="Cheque">Cheque</Option>
-                <Option value="Cash">Cash</Option>
-                <Option value="EFTPOS">EFTPOS</Option>
-              </Select>
-
-              <DatePicker size="large" onChange={handleChangeDate} />
-
-            </Input.Group>
-            <br />
-            <p />
-          </div>
-
-          <button className="button" name="addnew" type="submit">
-            <Link to="./AddPayroll"> <button> Add New Payroll</button></Link>
-          </button>
         </div>
 
         <div style={{ paddingTop: 10 }}>
           <Table dataSource={filteredList()}>
-            <Column title="Payroll ID" dataIndex="id" key="id" value="id" />
+            <Column title="Complaint ID" dataIndex="id" key="id" />
             <Column title="First Name" dataIndex="fname" key="firstName" />
             <Column title="Last Name" dataIndex="lname" key="lastName" />
-            <Column title="Amount" dataIndex="amount" key="amount" />
-            <Column title="Bonus" dataIndex="bonus" key="bonus" />
-            <Column title="Payment Method" dataIndex="paymethod" key="paymethod" />
-            <Column title="Pay Date" dataIndex="paydate" key="paydate" />
-            <Column title="Description" dataIndex="description" key="Description" />
-            <Column title="Options" key="id" render={(p) => (
-              <>
-                <Space split={<Divider type="vertical" />}>
-                  <Button id={p.id} onClick={handleDelete}> delete</Button>
-                  <Button id={p.id} onClick={handleEditRoute}>update</Button>
-                </Space>
-              </>
-            )} />
+            <Column title="Type" dataIndex="complainttype" key="complainttype" />
+            <Column
+              title="Description"
+              dataIndex="complaintdescription"
+              key="complaintdescription"
+            />
+            <Column title="Date Submitted" dataIndex="complaintdate" key="complaintdate" />
+            <Column title="Status" dataIndex="status" key="status" />
+            <Column
+              title="Options"
+              key="id"
+              render={p => (
+                <>
+                  <Space split={<Divider type="vertical" />}>
+                    <Button id={p.id} onClick={handleSolved}>
+                      {' '}
+                      Solved{' '}
+                    </Button>
+                  </Space>
+                </>
+              )}
+            />
           </Table>
         </div>
       </>
@@ -145,8 +147,8 @@ const Content = () => {
   }
 };
 
-const PayrollHistory = () => {
+const ComplaintList = () => {
   return <WebLayout content={Content()} />;
 };
 
-export default PayrollHistory;
+export default ComplaintList;
