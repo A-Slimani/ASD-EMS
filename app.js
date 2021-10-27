4fc4ce1d698ba84a230d2bae7c69c7606af42a25
AbdullahS98 <44589099+AbdullahS98@users.noreply.github.com>
2021-10-27 12:57:17 +1100

    added routing fix

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static(path.join('asd-ems-frontend', 'build')));
+app.use('*', express.static(path.join('asd-ems-frontend', 'build')))
 
 app.get('*', (request, response) => {
 	response.sendFile(path.join('./asd-ems-frontend', 'build', 'index.html'))
