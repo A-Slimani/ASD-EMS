3504c6f4bb63c751d3d78cdd0d7d9b09c9f8c250
AbdullahS98 <44589099+AbdullahS98@users.noreply.github.com>
2021-11-07 20:29:57 +1100

    minor changes

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(express.static(path.join('asd-ems-frontend', 'build')));
-app.use('*', express.static(path.join('asd-ems-frontend', 'build')))
+app.use(express.static(path.join('asd-ems-client', 'build')));
+app.use('*', express.static(path.join('asd-ems-client', 'build')));
 
-app.get('*', (request, response) => {
-	response.sendFile(path.join('./asd-ems-frontend', 'build', 'index.html'))
-})
+// app.get('*', (request, response) => {
+//   response.sendFile(path.join('./asd-ems-client', 'build', 'index.html'));
+// });
 
 app.listen(port, () => console.log(`Server is running on port ${port}!`));
