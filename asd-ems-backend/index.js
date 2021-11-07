64dcdb37eefee674aef23d672fb801cb69b34097
AbdullahS98 <44589099+AbdullahS98@users.noreply.github.com>
2021-11-07 20:51:20 +1100

    minor changes

--- asd-ems-backend/index.js
+++ asd-ems-backend/index.js
@@ -1,4 +1,4 @@
-const app = require('./App'); // actual Express appplication
+const app = require('./App'); // actual Express application
 const http = require('http');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
